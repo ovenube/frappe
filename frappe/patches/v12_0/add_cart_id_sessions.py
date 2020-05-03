0664f7a143c0bdce936363833e536b5afadc60a2
seethersan <carlos_jcez@hotmail.com>
2020-05-02 23:33:45 -0500

    [fix] added patches

--- frappe/patches/v12_0/add_cart_id_sessions.py
+++ frappe/patches/v12_0/add_cart_id_sessions.py
@@ -3,4 +3,4 @@ import frappe
 
 def execute():
 	if "device" not in frappe.db.get_table_columns("Sessions"):
-		frappe.db.sql("alter table tabSessions add column `cart_id` varchar(255) default 'desktop'")
+		frappe.db.sql("alter table tabSessions add column `cart_id` varchar(255) default NULL")
